3d316308049ba624aa151ea79c6a77597642da8c
Valeriy Voronets <Voronets.v@gmail.com>
2020-12-17 13:49:57 +0200

    создал разметку изображений

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
-import * as galeryItems from './galery-items.js'
+import galeryItems from './galery-items.js'
 const gallery = document.querySelector('.gallery');
 
-console.log(galeryItems)
+
+function getImage(arr) {
+    const list = 
+        arr.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+    <a class="gallery__link"
+    href= '${original}'>
+    <img class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"/>
+  </a>
+</li>` ).join('')
+    return list;
+}
+
+gallery.insertAdjacentHTML('beforeend', getImage(galeryItems));
+
+
+
+
